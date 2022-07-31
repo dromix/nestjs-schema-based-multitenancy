83e990f9960eed5c412a2ba9176b8f9faf60351c
Serhii Kaliuzhnyi <serhii.kaliuzhnyi@sigma.software>
2022-07-31 13:37:10 +0300

    Add database module & custom multitenancy provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { MenuModule } from './menu/menu.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
-  imports: [],
+  imports: [MenuModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
---
+++ src/database/prisma-client-manager.ts
@@ -0,0 +1,48 @@
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { Request } from 'express';
+import { config } from '../config';
+
+type Client = { [key: string]: PrismaClient };
+
+@Injectable()
+export class PrismaClientManager implements OnModuleDestroy {
+  private clients: Client = {};
+
+  getTenantId(request: Request): string {
+    // TODO: retrieve and return the tenant ID from the request object,
+    // eventually throw an exception if the ID is not valid
+    console.log('get tenant');
+    return String(request.query.tenantId);
+  }
+
+  getClient(request: Request): PrismaClient {
+    const tenantId = this.getTenantId(request);
+    let client = this.clients[tenantId];
+
+    if (!client) {
+      const databaseUrl = config.database.url.replace('public', tenantId);
+
+      client = new PrismaClient({
+        datasources: {
+          db: {
+            url: databaseUrl,
+          },
+        },
+      });
+
+      // setup prisma middlewares if any
+
+      this.clients[tenantId] = client;
+    }
+
+    return client;
+  }
+
+  async onModuleDestroy() {
+    // wait for every cached instance to be disposed
+    await Promise.all(
+      Object.values(this.clients).map((client) => client.$disconnect()),
+    );
+  }
+}

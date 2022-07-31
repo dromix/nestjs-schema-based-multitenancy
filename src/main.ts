94fda5d68cba80f624d1da9a3f691b128beed4a7
Serhii Kaliuzhnyi <serhii.kaliuzhnyi@sigma.software>
2022-07-31 13:39:44 +0300

    Add logger & swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { config } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log'],
+  });
+
+  const documentConfig = new DocumentBuilder()
+    .setTitle('Multi tenancy with Prisma')
+    .setDescription('Multi tenancy with Prisma')
+    .setVersion('1.0')
+    .addTag('MultiTenancy')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentConfig);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(config.server.port, () => {
+    console.log(`Server is running on port ${config.server.port}`);
+  });
 }
+
 bootstrap();
